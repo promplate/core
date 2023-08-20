e9164b1e80fcc32d1c5bca4a438f8c1707f44f82
CNSeniorious000 <wjj123345567@icloud.com>
2023-08-20 22:19:53 +0800

    implement `Chain` itself and integrate with OpenAI

--- python/promplate/chain/node.py
+++ python/promplate/chain/node.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractclassmethod
 from inspect import iscoroutinefunction
 from typing import Awaitable, Callable
 
@@ -10,18 +11,30 @@ Process = Callable[[Context], Context]
 AsyncProcess = Callable[[Context], Awaitable[Context]]
 
 
-class Node:
+class AbstractChain(ABC):
+    @abstractclassmethod
+    def run(self, context: Context, complete: Complete):
+        pass
+
+    @abstractclassmethod
+    async def arun(self, context: Context, complete: Complete | AsyncComplete):
+        pass
+
+
+class Node(AbstractChain):
     def __init__(
         self,
         template: Template,
         pre_processes: list[Process | AsyncProcess] | None = None,
         post_processes: list[Process | AsyncProcess] | None = None,
+        **config,
     ):
         self.template = template
         self.pre_processes = pre_processes or []
         self.post_processes = post_processes or []
+        self.run_config = config
 
-    def run(self, context: Context, complete: Complete):
+    def run(self, context, complete):
         for process in self.pre_processes:
             context = process(context)
 
@@ -29,14 +42,14 @@ class Node:
 
         assert isinstance(self.template, ChatTemplate) ^ isinstance(prompt, str)
 
-        result = {"__result__": complete(prompt)}
+        result = {"__result__": complete(prompt, **self.run_config)}
 
         for process in self.post_processes:
             result = process(result)
 
         return result
 
-    async def arun(self, context: Context, complete: Complete | AsyncComplete):
+    async def arun(self, context, complete):
         for process in self.pre_processes:
             if iscoroutinefunction(process):
                 context = await process(context)
@@ -48,9 +61,9 @@ class Node:
         assert isinstance(self.template, ChatTemplate) ^ isinstance(prompt, str)
 
         if iscoroutinefunction(complete):
-            result = {"__result__": await complete(prompt)}
+            result = {"__result__": await complete(prompt, **self.run_config)}
         else:
-            result = {"__result__": complete(prompt)}
+            result = {"__result__": complete(prompt, **self.run_config)}
 
         for process in self.post_processes:
             if iscoroutinefunction(process):
@@ -59,3 +72,41 @@ class Node:
                 context = process(context)
 
         return result
+
+    def next(self, chain: AbstractChain):
+        if isinstance(chain, Node):
+            return Chain(self, chain)
+        else:
+            return Chain(self, *chain)
+
+    def __add__(self, chain: AbstractChain):
+        return self.next(chain)
+
+
+class Chain(AbstractChain):
+    def __init__(self, *nodes: AbstractChain):
+        self.nodes = list(nodes)
+
+    def next(self, chain: AbstractChain):
+        if isinstance(chain, Node):
+            return Chain(*self, chain)
+        else:
+            return Chain(*self, *chain)
+
+    def __add__(self, chain):
+        return self.next(chain)
+
+    def __iter__(self):
+        return self.nodes
+
+    def run(self, context, complete):
+        for node in self.nodes:
+            context = node.run(context, complete)
+
+        return context
+
+    async def arun(self, context, complete):
+        for node in self.nodes:
+            context = await node.arun(context, complete)
+
+        return context
---
+++ python/promplate/llm/openai.py
@@ -0,0 +1,39 @@
+from openai import ChatCompletion, Completion
+
+from promplate.prompt.chat import Message
+
+
+def text_complete(**default_config):
+    def complete_text(text: str, **config) -> str:
+        config = default_config | config
+        res = Completion.create(prompt=text, **config)
+        return res["choices"][0]["text"]
+
+    return complete_text
+
+
+def async_text_complete(**default_config):
+    async def async_complete_text(text: str, **config) -> str:
+        config = default_config | config
+        res = await Completion.acreate(prompt=text, **config)
+        return res["choices"][0]["text"]
+
+    return async_complete_text
+
+
+def chat_complete(**default_config):
+    def complete_chat(messages: list[Message], **config) -> str:
+        config = default_config | config
+        res = ChatCompletion.create(messages=messages, **config)
+        return res["choices"][0]["message"]["content"]
+
+    return complete_chat
+
+
+def async_chat_complete(**default_config):
+    async def async_complete_chat(messages: list[Message], **config) -> str:
+        config = default_config | config
+        res = await ChatCompletion.acreate(messages=messages, **config)
+        return res["choices"][0]["message"]["content"]
+
+    return async_complete_chat
