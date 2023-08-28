294e8d21ca9e400ae851369abb84941dc5415b6d
CNSeniorious000 <wjj123345567@icloud.com>
2023-08-28 16:06:00 +0800

    `llm.openai` support streaming

--- python/promplate/llm/base.py
+++ python/promplate/llm/base.py
@@ -1,11 +1,17 @@
-from typing import Awaitable, Callable
+from typing import AsyncIterator, Awaitable, Callable, Iterator
 
 from promplate.prompt.chat import Message
 
 CompleteText = Callable[[str], str]
 CompleteChat = Callable[[list[Message]], str]
+GenerateText = Callable[[str], Iterator[str]]
+GenerateChat = Callable[[list[Message]], Iterator[str]]
 Complete = CompleteText | CompleteChat
+Generate = GenerateText | GenerateChat
 
 AsyncCompleteText = Callable[[str], Awaitable[str]]
 AsyncCompleteChat = Callable[[list[Message]], Awaitable[str]]
+AsyncGenerateText = Callable[[str], AsyncIterator[str]]
+AsyncGenerateChat = Callable[[list[Message]], AsyncIterator[str]]
 AsyncComplete = AsyncCompleteText | AsyncCompleteChat
+AsyncGenerate = AsyncGenerateText | AsyncGenerateChat
--- python/promplate/llm/openai.py
+++ python/promplate/llm/openai.py
@@ -7,8 +7,8 @@ from .base import *
 def text_complete(**default_config) -> CompleteText:
     def complete_text(text: str, **config):
         config = default_config | config | {"stream": False, "prompt": text}
-        res = Completion.create(**config)
-        return res["choices"][0]["text"]
+        result = Completion.create(**config)
+        return result["choices"][0]["text"]
 
     return complete_text
 
@@ -16,17 +16,37 @@ def text_complete(**default_config) -> CompleteText:
 def async_text_complete(**default_config) -> AsyncCompleteText:
     async def async_complete_text(text: str, **config):
         config = default_config | config | {"stream": False, "prompt": text}
-        res = await Completion.acreate(**config)
-        return res["choices"][0]["text"]
+        result = await Completion.acreate(**config)
+        return result["choices"][0]["text"]
 
     return async_complete_text
+
+
+def text_generate(**default_config) -> GenerateText:
+    def generate_text(text: str, **config):
+        config = default_config | config | {"stream": True, "prompt": text}
+        stream = Completion.create(**config)
+        for event in stream:
+            yield event["choices"][0]["text"]
+
+    return generate_text
+
+
+def async_text_generate(**default_config) -> AsyncGenerateText:
+    async def async_generate_text(text: str, **config):
+        config = default_config | config | {"stream": True, "prompt": text}
+        stream = Completion.acreate(**config)
+        async for event in stream:
+            yield event["choices"][0]["text"]
+
+    return async_generate_text
 
 
 def chat_complete(**default_config) -> CompleteChat:
     def complete_chat(messages: list[Message], **config):
         config = default_config | config | {"stream": False, "messages": messages}
-        res = ChatCompletion.create(messages=messages, **config)
-        return res["choices"][0]["message"]["content"]
+        result = ChatCompletion.create(**config)
+        return result["choices"][0]["message"]["content"]
 
     return complete_chat
 
@@ -34,7 +54,29 @@ def chat_complete(**default_config) -> CompleteChat:
 def async_chat_complete(**default_config) -> AsyncCompleteChat:
     async def async_complete_chat(messages: list[Message], **config):
         config = default_config | config | {"stream": False, "messages": messages}
-        res = await ChatCompletion.acreate(messages=messages, **config)
-        return res["choices"][0]["message"]["content"]
+        result = await ChatCompletion.acreate(**config)
+        return result["choices"][0]["message"]["content"]
 
     return async_complete_chat
+
+
+def chat_generate(**default_config) -> GenerateChat:
+    def generate_chat(messages: list[Message], **config):
+        config = default_config | config | {"stream": True, "messages": messages}
+        stream = ChatCompletion.create(**config)
+        for event in stream:
+            delta: dict = event["choices"][0]["delta"]
+            yield delta.get("content", "")
+
+    return generate_chat
+
+
+def async_chat_generate(**default_config) -> AsyncGenerateChat:
+    async def async_generate_chat(messages: list[Message], **config):
+        config = default_config | config | {"stream": True, "messages": messages}
+        stream = ChatCompletion.acreate(**config)
+        async for event in stream:
+            delta: dict = event["choices"][0]["delta"]
+            yield delta.get("content", "")
+
+    return async_generate_chat
