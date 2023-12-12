8152a737eac46c4bab92a77dcdba62cd613c749d
Muspi Merol <wjj123345567@icloud.com>
2023-12-12 22:12:50 +0800

    feat: improve performance and fix `pypy` compatibility

--- python/promplate/prompt/utils.py
+++ python/promplate/prompt/utils.py
@@ -1,7 +1,7 @@
 from functools import cached_property, wraps
 from inspect import currentframe
 from re import compile
-from typing import Callable, ParamSpec, TypeVar
+from typing import Any, Callable, ParamSpec, TypeVar
 
 split_template_tokens = compile(
     r"(\s{%-.*?-%}\s|\s{{-.*?-}}\s|\s{#-.*?-#}\s|\s{%-.*?%}|\s{{-.*?}}|\s{#-.*?#}|{%.*?-%}\s|{{.*?-}}\s|{#.*?-#}\s|{%.*?%}|{{.*?}}|{#.*?#})"
@@ -86,6 +86,5 @@ def cache_once(func: Callable[P, T]) -> Callable[P, T]:
 
 
 @cache_once
-def get_clean_global_builtins():
-    exec("", env := {})
-    return env["__builtins__"]
+def get_clean_global_builtins() -> dict[str, Any]:
+    return __builtins__ if isinstance(__builtins__, dict) else __builtins__.__dict__
